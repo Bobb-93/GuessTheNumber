8c82a4b5e78bcff019cf2ca6e7f8e32e1622aa77
Bobb-93 <borisiakimov@gmail.com>
2021-03-24 18:29:13 +0200

    game rewriten using jquery to some extent

--- js/index.js
+++ js/index.js
@@ -1,26 +1,28 @@
 // dom: various dom objects
 const dom = {
-    playButton: document.getElementById("playButton"),
-    guessButton: document.getElementById("guessButton"),
-    resetButton: document.getElementById("resetButton"),
-    startNewGameButton: document.getElementById("startNewGameButton"),
-    selectLevelDiv: document.getElementById("selectLevelDiv"),
-    guessTheNumberDiv: document.getElementById("guessTheNumberDiv"),
-    guessTableDiv: document.getElementById("guessTableDiv"),
-    numberOfTriesRemaining: document.getElementById("numberOfTriesRemaining"),
-    numberOfTries: document.getElementById("numberOfTries"),
-    guessingInput: document.getElementById("guessingInput"),
-    from: document.getElementById("from"),
-    to: document.getElementById("to"),
-    dropDown: document.querySelector("label[for=selectLevelDropDown]"),
-    selectLevelDropDown: document.getElementById("selectLevelDropDown"),
-    playingLevel: document.getElementById("playingLevel"),
-    endMessage: document.getElementById("endMessage"),
-    level: document.getElementById("level"),
-    guessTable: document.getElementById("guess-table")
+    playButton: $("#playButton"),
+    guessButton: $("#guessButton"),
+    resetButton: $("#resetButton"),
+    startNewGameButton: $("#startNewGameButton"),
+    selectLevelDiv: $("#selectLevelDiv"),
+    guessTheNumberDiv: $("#guessTheNumberDiv"),
+    guessTableDiv: $("#guessTableDiv"),
+    numberOfTriesRemaining: $("#numberOfTriesRemaining"),
+    numberOfTries: $("#numberOfTries"),
+    guessingInput: $("#guessingInput"),
+    from: $("#from"),
+    to: $("#to"),
+    dropDown: $("label[for='selectLevelDropDown']"),
+    selectLevelDropDown: $("#selectLevelDropDown"),
+    playingLevel: $("#playingLevel"),
+    endMessage: $("#endMessage"),
+    level: $("#level"),
+    guessTable: $("#guess-table"),
+    tbody: $("#guess-table > tbody")
 };
+console.log(dom.guessTable);
+dom.tbodyRef = dom.tbody[0];
 
-dom.tbodyRef = dom.guessTable.tBodies[0];
 // alternative way for tbodyRef:
 // dom.tbodyRef = dom.guessTable.getElementsByTagName("tbody")[0];
 
@@ -45,36 +47,36 @@ let gameVariables = {
 };
 
 function playGame(){
-    gameVariables.difficulty = dom.selectLevelDropDown.value;
+    gameVariables.difficulty = dom.selectLevelDropDown.val();
     // console.log(`gameVariables.difficulty = ${gameVariables.difficulty}`);
     if(gameVariables.difficulty === "easy"){
         gameVariables.maxValue = 10;
-        dom.level.innerText = "Easy";
+        dom.level.text("Easy");
     }else if(gameVariables.difficulty === "basic"){
         gameVariables.maxValue = 50;
-        dom.level.innerText = "Basic";
+        dom.level.text("Basic");
     }else{
         gameVariables.maxValue = 100;
-        dom.level.innerText = "Advanced";
+        dom.level.text("Advanced");
     }
 
     gameVariables.maxTries = Math.ceil(Math.log2(gameVariables.maxValue
                              - gameVariables.minValue + 1)) + 1;
     gameVariables.tries = gameVariables.maxTries;
-    dom.numberOfTriesRemaining.innerText = gameVariables.maxTries;
-    dom.numberOfTries.innerText = gameVariables.maxTries;
-    dom.from.innerText=`${gameVariables.minValue}`;
-    dom.to.innerText=`${gameVariables.maxValue}`;
+    dom.numberOfTriesRemaining.text(gameVariables.maxTries);
+    dom.numberOfTries.text(gameVariables.maxTries);
+    dom.from.text(`${gameVariables.minValue}`);
+    dom.to.text(`${gameVariables.maxValue}`);
     gameVariables.randomNumber =  Math.floor(Math.random()
         * (gameVariables.maxValue - gameVariables.minValue + 1))
         + gameVariables.minValue;
     // console.log(randomNumber);
-    dom.selectLevelDropDown.style.display = "none";
-    dom.playButton.style.display = "none";
-    dom.dropDown.style.display = "none";
-    dom.playingLevel.style.display = "block";
-    // dom.endMessage.style.display = "block";
-    dom.guessTheNumberDiv.style.display = "block";
+    dom.selectLevelDropDown.css("display", "none");
+    dom.playButton.css("display", "none");
+    dom.dropDown.css("display", "none");
+    dom.playingLevel.css("display", "block");
+    // dom.endMessage.css("display", "block");
+    dom.guessTheNumberDiv.css("display", "block");
     dom.guessingInput.focus();
 }
 
@@ -89,7 +91,7 @@ function checkNumber(){
         insertedText: undefined
     };
     tableVariables.guessNumber = Number(guessingInput.value);
-    dom.guessTableDiv.style.display = "block";
+    dom.guessTableDiv.css("display", "block");
     if(tableVariables.guessNumber < gameVariables.minValue
         || tableVariables.guessNumber > gameVariables.maxValue){
         alert(
@@ -98,7 +100,7 @@ function checkNumber(){
         //prevent displaying of table, if our first guess is a number,
         //that is out of range
         if(gameVariables.count === 1){
-            dom.guessTableDiv.style.display = "none";
+            dom.guessTableDiv.css("display", "none");
         }
         guessingInput.value = "";
         dom.guessingInput.focus();
@@ -106,7 +108,7 @@ function checkNumber(){
             && gameVariables.count < gameVariables.maxTries){
         gameVariables.count += 1;
         gameVariables.tries -= 1;
-        dom.numberOfTriesRemaining.innerText = gameVariables.tries;
+        dom.numberOfTriesRemaining.text(gameVariables.tries);
         //NOT working:
         // tableRows[tableRows.length-1].insertAdjacentHTML("afterbegin",
         //`<tr><td>${guessNumber}</td><td>high</td></tr>`);
@@ -125,7 +127,7 @@ function checkNumber(){
             && gameVariables.count < gameVariables.maxTries){
         gameVariables.count += 1;
         gameVariables.tries -= 1;
-        dom.numberOfTriesRemaining.innerText = gameVariables.tries;
+        dom.numberOfTriesRemaining.text(gameVariables.tries);
         //NOT working:
         // tableRows[tableRows.length-1].insertAdjacentHTML("afterbegin",
         //`<tr><td>${guessNumber}</td><td>low</td></tr>`);
@@ -149,12 +151,12 @@ function checkNumber(){
         tableVariables.newCellNumber.appendChild(tableVariables.insertedText);
         tableVariables.insertedText = document.createTextNode(`win`);
         tableVariables.newCellText.appendChild(tableVariables.insertedText);
-        dom.guessTheNumberDiv.style.display = "none";
-        dom.playingLevel.style.display = "none";
-        dom.endMessage.style.display = "block";
-        dom.endMessage.innerText
-            = `Bravo! You guessed my number (${gameVariables.randomNumber}) from ${gameVariables.count} tries`;
-        dom.startNewGameButton.style.display = "block";
+        dom.guessTheNumberDiv.css("display", "none");
+        dom.playingLevel.css("display", "none");
+        dom.endMessage.css("display", "block");
+        dom.endMessage.text(
+            `Bravo! You guessed my number (${gameVariables.randomNumber}) from ${gameVariables.count} tries`);
+        dom.startNewGameButton.css("display", "block");
     }else{
         tableVariables.newRow = dom.tbodyRef.insertRow(0);
         tableVariables.newCellNumber = tableVariables.newRow.insertCell();
@@ -164,12 +166,12 @@ function checkNumber(){
         tableVariables.newCellNumber.appendChild(tableVariables.insertedText);
         tableVariables.insertedText = document.createTextNode(`loss`);
         tableVariables.newCellText.appendChild(tableVariables.insertedText);
-        dom.guessTheNumberDiv.style.display = "none";
-        dom.playingLevel.style.display = "none";
-        dom.endMessage.style.display = "block";
-        dom.endMessage.innerText
-            = `You lose! My number was (${gameVariables.randomNumber})`;
-        dom.startNewGameButton.style.display = "block";
+        dom.guessTheNumberDiv.css("display", "none");
+        dom.playingLevel.css("display", "none");
+        dom.endMessage.css("display", "block");
+        dom.endMessage.text(
+            `You lose! My number was (${gameVariables.randomNumber})`);
+        dom.startNewGameButton.css("display", "block");
     }
 }
 
@@ -180,20 +182,20 @@ function startNewGame(){
     // gameVariables.tries = undefined;
     // gameVariables.maxTries = undefined;
     // gameVariables.difficulty = undefined;
-    dom.guessTheNumberDiv.style.display = "none";
-    dom.playingLevel.style.display = "none";
-    dom.selectLevelDropDown.style.display = "inline-block";
-    dom.dropDown.style.display = "inline";
-    dom.playButton.style.display = "inline-block";
-    dom.guessTableDiv.style.display = "none";
+    dom.guessTheNumberDiv.css("display", "none");
+    dom.playingLevel.css("display", "none");
+    dom.selectLevelDropDown.css("display", "inline-block");
+    dom.dropDown.css("display", "inline");
+    dom.playButton.css("display", "inline-block");
+    dom.guessTableDiv.css("display", "none");
     document.querySelectorAll("table tbody tr").forEach(function(e){
         e.remove();
     });
     dom.level.innerHTML = "";
-    dom.startNewGameButton.style.display="none";
-    dom.guessingInput.value="";
-    dom.endMessage.style.display = "none";
-    dom.endMessage.innerText = "";
+    dom.startNewGameButton.css("display", "none");
+    dom.guessingInput.val("");
+    dom.endMessage.css("display", "none");
+    dom.endMessage.text("");
 }
 
 function resetGame(){
@@ -202,10 +204,10 @@ function resetGame(){
     }
 }
 
-dom.playButton.addEventListener("click", playGame);
-dom.guessButton.addEventListener("click", checkNumber);
-dom.resetButton.addEventListener("click", resetGame);
-dom.startNewGameButton.addEventListener("click", startNewGame);
+dom.playButton.on("click", playGame);
+dom.guessButton.on("click", checkNumber);
+dom.resetButton.on("click", resetGame);
+dom.startNewGameButton.on("click", startNewGame);
 
 //From https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
 // Execute a function when the user releases a key on the keyboard
